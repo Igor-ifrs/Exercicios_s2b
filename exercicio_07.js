3f45996f7eda1d2e357fea935ed814ebabb38cbe
Igor-ifrs <cigor80@gmail.com>
2018-04-28 22:06:33 -0300

    Exercicio 7 resolvido com expressão regular

--- exercicio_07.js
+++ exercicio_07.js
@@ -2,10 +2,27 @@
 7. Escreva uma função toMaiusculaPrimeira(s) que recebe uma string s(assuma qualquer string não vazia) e
 retorna a mesma string com a primeira letra em maiúscula.
 */
-let toMaiusculaPrimeira = function (string) {
-    string = [...string.toLowerCase()];//  só pra garantir todas as letras minusculas
-    string[0] = string[0].toUpperCase();
-    return string.join('');    
+let toMaiusculaPrimeira = function (texto) {
+    texto = [...texto.toLowerCase()]; //  só pra garantir todas as letras minusculas
+    texto[0] = texto[0].toUpperCase();
+    return texto.join('');
 }
 console.log(toMaiusculaPrimeira('IGOR CORREA'));
+
+let toMaiusculaPrimeira2 = function (texto) {
+    texto = texto.toLowerCase();
+    return texto[0].toUpperCase() + texto.slice(1);
+};
+const string = 'a varias formas de fazer A MESMA COISA EM JAVASCRIPT';
+
+console.log(toMaiusculaPrimeira2(string));
+
+let capitalize = function (texto) {
+    texto = texto.toLowerCase();
+    return texto.replace(/\w\S*/g, function (t) {
+        return t.charAt(0).toUpperCase() + t.substr(1).toLowerCase();
+    });
+}
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+console.log(capitalize(string));
 
